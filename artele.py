1d19bf602c1ad1be7dab181ee1efe43b148bc6a4
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-16 20:58:00 +0530

    Update artele.py

--- artele.py
+++ artele.py
@@ -4,17 +4,16 @@ from telethon import TelegramClient, events
 # this is secret, do not share with anyone
 api_id = 14456079
 api_hash = '51da94efc990b58e3db3c897fc24e8d6'
-
-phone = '+61482091907'
-password = 'YOUR_PASSWORD'  #use this if you have two factor authentication turned on for your account
-session_file = 'oflilelly89' #this is where your session data will persist. You can name the file anything you want.
+string =''
+ #this is where your session data will persist. You can name the file anything you want.
 
 message = '**AUTO REPLY** \n\nHey baby!!\n\nSorry Im not answering private msgs right now but I have everything you need. So if youre looking for a sexy fun time, I can provide it. Come & join me at my website, get my exclusive naughty content & lets chat 1-on-1 together.\n\nSee you there,\n-elly xoxo\n\n https://elly.onepage.me \n\n**AUTO REPLY**'
 
 if __name__ == '__main__':
     #Create the client
     # use sequential_updates=True to respond to messages one at a time
-    client  = TelegramClient(session_file, api_id, api_hash, sequential_updates=True)  
+    client  = TelegramClient(StringSession(string), api_id, api_hash)
+
 
     @client.on(events.NewMessage(incoming=True)) #handle only incoming messages
     async def handle_new_message(event):
