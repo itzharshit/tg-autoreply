460df375d47643184d65de241c58ce1cd2b15ebe
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-16 22:33:59 +0530

    Update artele.py

--- artele.py
+++ artele.py
@@ -8,7 +8,7 @@ from telethon.sessions import StringSession
 # this is secret, do not share with anyone
 api_id = 14456079
 api_hash = '51da94efc990b58e3db3c897fc24e8d6'
-string = getenv("STRING")
+string = getenv("STRING", "1ApWapzMBuyN3ruAfNvvaA3eG5tSs57G_aEsg2LyaHOLXhzauhJ_DXh-rr3TUVKN2sYM_lXkYwKapP5ckxpxPQl62a2cCbIlkaoBhTlGJB4EfDFZflwMr6QQWPyuVWxtDUWiBVX0-LGfY96OoxGcjVMPKn6EjVlGIq2XAXYkscJtIz2keyDn05usfHsOIxL-dc6XVESryq9yZ81US3z8UC5mgtTpgGlim9sghol_RoL_TziirvjoIUJGS9eVBnfLlNm_Aupt0B4dyA-cX2Cv4sqCZjm6RB92AeFvmGq5lvZz9A1AHpqHFgB-PDgvc0_Beq0qfNrQhn0bXjNd3HNTJWn40NM4hHNY=")
  #this is where your session data will persist. You can name the file anything you want.
 message = '**Hello sir, this is a auto replied message. I am currently offline.**\n\nIf you want to know the price of premium save restricted contents bot, you can check it below\n\n✓1 Week - 6$(USD)\n✓ 1 Month - $13(USD) \nPayment mode is any crypto (BTC __preferable__)\n\nIf you want to subscribe premium feature you can reply otherwise you are free from here.'
 
